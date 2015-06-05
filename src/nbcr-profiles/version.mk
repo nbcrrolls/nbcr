c16678ad91d9e8142ca8ef20e7e4c124a2771493
nadyawilliams <nadya@sdsc.edu>
2015-06-05 14:10:33 -0700

    create  nbcr-etc rpm for frontend's install

---
+++ src/nbcr-profiles/version.mk
@@ -0,0 +1,11 @@
+NAME       = nbcr-profiles
+VERSION    = 1
+RELEASE    = 0
+PKGROOT    = /opt/modulefiles
+
+GNUNAME    = gnu
+GNUVERSION = 4.4.7
+
+RPM.EXTRAS = AutoReq:No
+RPM.REQUIRES    = environment-modules
+
